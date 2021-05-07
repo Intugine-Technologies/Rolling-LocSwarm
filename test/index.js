22cfd9587e51608611a0eacc367593e932cf01a6
freakymittal <mayank_mittal@outlook.com>
2021-05-07 12:37:36 +0530

    test: add a new test case to verify, that there is no zero duration in the halt calculation result

--- test/index.js
+++ test/index.js
@@ -1,16 +1,22 @@
 const assert = require('assert');
 const calc_halts = require('../index.js').all_halts;
-const test1_data = require('./test1.json');
+const data_sets = require('./data_sets/index.js');
 
 describe("Calculate Halts", () => {
 	describe("Default conditions", () => {
 		it("Should have length 11", () => {
-			const halts = calc_halts(test1_data.map(i => ({
+			const halts = calc_halts(data_sets.set1.map(i => ({
 			    loc: i.loc || i.gps,
 			    time: new Date(i.time || i.createdAt)
 			})), 1000);
-			console.log(halts);
 			assert.equal(halts.length, 11);
+		});
+		it("Should have no zero duration halts", () => {
+			const halts = calc_halts(data_sets.set2.map(i => ({
+			    loc: i.loc || i.gps,
+			    time: new Date(i.time || i.createdAt)
+			})), 200);
+			assert.equal(halts.filter(k => k.duration === 0).length, 0);
 		});
 	})
 });
