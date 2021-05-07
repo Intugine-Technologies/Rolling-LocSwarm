a7fa797d15de3d0cf6eba9662a638b3a974abacc
freakymittal <mayank_mittal@outlook.com>
2021-05-07 19:18:22 +0530

    patch: remove lodash and do sorting by vanilla js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const distance = require('@intugine-technologies/distance')
-const _ = require('lodash')
+const distance = require('@intugine-technologies/distance');
 
 const in_geofence = (loc1, loc2, __distance) =>
-	distance(loc1, loc2) <= __distance
+	distance(loc1, loc2) <= __distance;
 const main = (
 	__latest_ping,
 	__last_ping,
@@ -13,15 +12,15 @@ const main = (
 		...k,
 		to_time: new Date(k.to_time).valueOf(),
 		from_time: new Date(k.from_time).valueOf(),
-	}))
+	}));
 	const last_ping = __last_ping
 		? { ...__last_ping, time: new Date(__last_ping.time).valueOf() }
-		: null
+		: null;
 	const latest_ping = {
 		...__latest_ping,
 		time: new Date(__latest_ping.time).valueOf(),
-	}
-	const last_halt = halts.slice(-1)[0]
+	};
+	const last_halt = halts.slice(-1)[0];
 	if (
 		last_ping &&
 		last_halt &&
@@ -29,9 +28,9 @@ const main = (
 		last_halt.from_time < last_ping.time
 	) {
 		if (in_geofence(last_halt.loc, latest_ping.loc, distance_threshold)) {
-			halts[halts.length - 1].to_time = latest_ping.time
+			halts[halts.length - 1].to_time = latest_ping.time;
 			halts[halts.length - 1].duration =
-				latest_ping.time - last_halt.from_time
+				latest_ping.time - last_halt.from_time;
 		}
 	} else if (last_ping && last_ping.time !== latest_ping.time) {
 		if (in_geofence(last_ping.loc, latest_ping.loc, distance_threshold)) {
@@ -40,25 +39,28 @@ const main = (
 				to_time: latest_ping.time,
 				duration: latest_ping.time - last_ping.time,
 				loc: last_ping.loc,
-			})
+			});
 		}
 	}
 	return halts.map((k) => ({
 		...k,
 		from_time: new Date(k.from_time),
 		to_time: new Date(k.to_time),
-	}))
-}
+	}));
+};
 
 const all_halts = (__locations = [], distance_threshold = 1000) => {
-	let halts = []
-	_.sortBy(__locations, 'time').forEach((k, kdx, array) => {
-		halts = main(k, kdx === 0 ? null : array[kdx - 1], 1000, halts)
-	})
-	return halts
-}
+	let halts = [];
+	__locations
+		.slice()
+		.sort((x, y) => x.time - y.time)
+		.forEach((k, kdx, array) => {
+			halts = main(k, kdx === 0 ? null : array[kdx - 1], 1000, halts);
+		});
+	return halts;
+};
 
 module.exports = {
 	all_halts: all_halts,
 	rolling_halt: main,
-}
+};
