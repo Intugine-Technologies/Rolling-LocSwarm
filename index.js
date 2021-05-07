a599719a8d80890f1d791d0d98dbf51d3dba80aa
freakymittal <mayank_mittal@outlook.com>
2021-05-07 12:38:29 +0530

    fix: zero duration halts and duration inconsistency issue

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-// const Joi = require('joi');
 const distance = require("@intugine-technologies/distance");
+const _ = require("lodash");
 
-// const schema = require('./schema');
 const in_geofence = (loc1, loc2, __distance) =>
 	distance(loc1, loc2) <= __distance;
 const main = (
-	latest_ping,
-	last_ping,
+	__latest_ping,
+	__last_ping,
 	distance_threshold = 1000,
 	prev_halts = []
 ) => {
-	// try{
-	// 	//Check schema
-	// 	Joi.assert(undefined, schema.ping)
-	// } catch(e){
-	// 	throw e;
-	// }
-
 	const halts = prev_halts.map((k) => ({
 		...k,
+		to_time: new Date(k.to_time).valueOf(),
+		from_time: new Date(k.from_time).valueOf(),
 	}));
+	const last_ping = __last_ping
+		? { ...__last_ping, time: new Date(__last_ping.time).valueOf() }
+		: null;
+	const latest_ping = {
+		...__latest_ping,
+		time: new Date(__latest_ping.time).valueOf(),
+	};
 	const last_halt = halts.slice(-1)[0];
 	if (
 		last_ping &&
@@ -27,28 +28,31 @@ const main = (
 		last_halt.to_time === last_ping.time &&
 		last_halt.from_time < last_ping.time
 	) {
-		if (in_geofence(last_ping.loc, latest_ping.loc, distance_threshold)) {
+		if (in_geofence(last_halt.loc, latest_ping.loc, distance_threshold)) {
 			halts[halts.length - 1].to_time = latest_ping.time;
-			halts[halts.length - 1].duration =
-				latest_ping.time.valueOf() - last_halt.from_time.valueOf();
+			halts[halts.length - 1].duration = latest_ping.time - last_halt.from_time;
 		}
-	} else if (last_ping) {
+	} else if (last_ping && last_ping.time !== latest_ping.time) {
 		if (in_geofence(last_ping.loc, latest_ping.loc, distance_threshold)) {
 			halts.push({
 				from_time: last_ping.time,
 				to_time: latest_ping.time,
-				duration: latest_ping.time.valueOf() - last_ping.time.valueOf(),
+				duration: latest_ping.time - last_ping.time,
 				loc: last_ping.loc,
 			});
 		}
 	}
-	return halts;
+	return halts.map((k) => ({
+		...k,
+		from_time: new Date(k.from_time),
+		to_time: new Date(k.to_time),
+	}));
 };
 
-const all_halts = (locations = [], distance_threshold = 1000) => {
+const all_halts = (__locations = [], distance_threshold = 1000) => {
 	let halts = [];
-	locations.forEach((k, kdx) => {
-		halts = main(k, kdx === 0 ? null : locations[kdx - 1], 1000, halts);
+	_.sortBy(__locations, "time").forEach((k, kdx, array) => {
+		halts = main(k, kdx === 0 ? null : array[kdx - 1], 1000, halts);
 	});
 	return halts;
 };
