4f935a2036bf75eb76b6c9637fc10bbcbee9685d
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2022-02-26 17:33:52 +0530

    test: add testcase for stateful halt calculation

--- test/data_sets/index.js
+++ test/data_sets/index.js
@@ -2,4 +2,5 @@ module.exports = {
 	set1: require('./test1.json'),
 	set2: require('./test2.json'),
 	set3: require('./test3.json'),
+	set4: require('./test4.json'),
 };
--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 const assert = require('chai').assert;
-const calc_halts = require('../index.js').all_halts;
+const locswarm = require('../index.js');
+const calc_halts = locswarm.all_halts;
+const calculateAllHaltState = locswarm.stateful.all_halts;
 const data_sets = require('./data_sets/index.js');
 
 describe('Calculate Halts', () => {
@@ -135,3 +137,25 @@ describe('Calculate Halts, with test data', () => {
 		});
 	});
 });
+
+
+describe('Calculate stateful halt calculation',() => {
+	describe('custom thresholds',() => {
+		const testCase = data_sets.set4;
+		const {
+			locations,
+			distance_threshold,
+			duration_threshold,
+		} = testCase.input;
+		const deepAssertionValue = testCase.results.halts.map(haltObj => {
+			haltObj.from_time = new Date(haltObj.from_time);
+			haltObj.to_time = new Date(haltObj.to_time);
+			return haltObj;
+		});
+		it(testCase.description,() => {
+			const {halts} = calculateAllHaltState(locations,distance_threshold,duration_threshold,[],null);
+
+			assert.deepEqual(deepAssertionValue,halts);
+		})
+	})
+})
